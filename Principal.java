d364a88fe63ab2945e2b188f3b1a6b7a8cdc7361
Guza xt <gustavomom1227@gmail.com>
2023-10-24 14:56:31 +0000

    salvo

--- Principal.java
+++ Principal.java
@@ -1,6 +1,6 @@
 public class Principal {
     
-    public static void main(String args){
+    public static void main(String[] args){
 
         Carro car = new Carro();
         Carro carAro = new Carro(17);
@@ -8,7 +8,7 @@ public class Principal {
         Carro carQuatroConstrutores = new Carro(15, "Azul Calcinha", "Uno Mile", 15300);
 
         System.out.println(car);
-        System.out.println(carAro);
+        System.out.println(carAro.getAro());
         System.out.println(carTresConstrutores);
         System.out.println(carQuatroConstrutores);
     }
